a441e1574c94fc3787540e37aa4576d777a0d2e8
aThebigbot <aThebigbot@users.noreply.github.com>
2022-08-30 09:35:22 +0200

    components(Introduction): builded

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
-
+  import Introduction from "./components/Introduction.svelte";
 </script>
 
 <main>
-  
+  <div>
+    <Introduction />
+  </div>
 </main>
 
 <style>
-
+* {
+  color: rgb(230, 216, 216);
+  font-family: "Roboto", sans-serif;
+  font-size: 1.2rem;
+  font-weight: 400;
+  line-height: 1.5;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+  margin-top: 25%;
+}
 </style>
--- src/components/Introduction.svelte
+++ src/components/Introduction.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
-
 </script>
 
 <main>
-  
+    <!-- URL: https://avatars.githubusercontent.com/u/77632836?v=4 -->
+    <img
+        src="https://avatars.githubusercontent.com/u/77632836?v="
+        alt="av"
+        class="av"
+        title="ME!!!!!!!"
+    />
+    <h1><span class="name">Thebigbot</span></h1>
+    <p>Hello! I'm Thebigbot, a young developer from italy</p>
 </main>
 
 <style>
-
+    .av {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        border: 2px solid #000;
+        filter: drop-shadow(0 0 3em #d85e0d) drop-shadow(0 0 0.5em #c75a12);
+    }
 </style>
