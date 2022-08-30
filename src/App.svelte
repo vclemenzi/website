b71dd0d8a8f60e44bf8cee098272335c00d65c90
aThebigbot <aThebigbot@users.noreply.github.com>
2022-08-30 14:18:37 +0200

    fix(*): position

--- src/App.svelte
+++ src/App.svelte
@@ -22,6 +22,9 @@
     margin: 0;
     padding: 0;
     text-align: center;
-    margin-top: 20%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 </style>
