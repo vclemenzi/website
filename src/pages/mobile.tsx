f3d0b304a4892e2b62f618742aee12984c69a97b
Thebigbot <77632836+athebigbot@users.noreply.github.com>
2023-01-06 15:56:21 +0100

    new style

--- src/pages/mobile.tsx
+++ src/pages/mobile.tsx
@@ -2,24 +2,8 @@
 import { IconBrandGithub, IconAt, IconMapPin, IconClock } from "@tabler/icons";
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useEffect } from "react";
 
-
-const Home: NextPage = () => {
-
-  useEffect(() => {
-    const ua = navigator.userAgent;
-    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-      window.location.href = "/mobile";
-    }
-    if (
-      /Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
-        ua
-      )
-    ) {
-      window.location.href = "/mobile";
-    }
-  }, []);
+const HomeMobile: NextPage = () => {
 
   return (
     <>
@@ -31,18 +15,16 @@ const Home: NextPage = () => {
 
       <main>
         {/*<nav className="rounded-md border bg-red-600 p-5">a</nav>*/}
-        <div className="flex">
-          <div className="inline">
-            <div className="mr-9 h-56 w-56 rounded-md border-2">
-              <img
-                src="/favicon.ico"
-                className="m-8 h-40 w-40 rounded-full"
-                alt="image"
-              />
-            </div>
+        <div>
+          <div className=" flex justify-center">
+            <img
+              src="/favicon.ico"
+              className="m-8 h-40 w-40 rounded-full border-2"
+              alt="image"
+            />
           </div>
-          <div className="inline">
-            <div className="m-5 flex h-28 w-[26rem] justify-center rounded-md border-2 p-5">
+          <div>
+            <div className="m-5 flex h-28 justify-center rounded-md border-2 p-5">
               <IconBrandGithub
                 className="m-4 cursor-pointer hover:scale-110"
                 size={35}
@@ -52,18 +34,42 @@ const Home: NextPage = () => {
                 size={35}
               />
             </div>
-
-            <div className="m-5 flex h-28 w-[26rem] justify-center rounded-md border-2 p-5">
+            <div className="m-5 flex rounded-md border-2 p-5 text-justify">
               <div>
-                <h1 className="text-4xl m-4">?</h1>
+                <h1 className="mb-2 text-xl">About Me</h1>
+                <p className="text-md">
+                  Hi! My name is Thebigbot, and I am a web developer with 2
+                  years of experience in the field. I specialize in React ecc..,
+                  and have a passion for creating intuitive and user-friendly
+                  websites and applications. Contact me for any question! :)
+                </p>
+              </div>
+            </div>
+            <div className="m-5 flex h-28  justify-center rounded-md border-2 p-5">
+              <div>
+                <h1 className="m-4 text-4xl">?</h1>
               </div>
             </div>
           </div>
         </div>
+        <div>
+          <div className="m-5 flex  justify-center rounded-md border-2 p-5">
+            <div className="flex">
+              <span className="mr-10 flex">
+                <IconMapPin className="mr-2 inline-block" />{" "}
+                <p className="inline-block cursor-default">Italy</p>
+              </span>
 
-        <div className="flex">
-          <div className="inline">
-            <div className="m-5 h-72 w-56 rounded-md border-2">
+              <span className="flex">
+                <IconClock className="mr-2 inline-block " />{" "}
+                <p className="inline-block cursor-default">UTC +01:00</p>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div className="m-5 flex  h-72 justify-center rounded-md border-2">
               <div className="m-8 h-40 w-40">
                 <h1 className="text-xl">Favorite Stacks</h1>
                 <br />
@@ -96,7 +102,7 @@ const Home: NextPage = () => {
               </div>
             </div>
 
-            <div className="m-5 h-72 w-56 rounded-md border-2">
+            <div className="m-5 flex h-72 justify-center rounded-md border-2">
               <div className="m-8 h-40 w-40">
                 <h1 className="text-xl">Favorite Apps</h1>
                 <br />
@@ -126,34 +132,6 @@ const Home: NextPage = () => {
                     Alacritty
                   </button>
                 </div>
-              </div> 
-            </div>
-          </div>
-          <div className="inline">
-            <div className="m-5 flex w-[26rem] justify-center rounded-md border-2 p-5">
-              <div className="flex">
-                <span className="mr-10 flex">
-                  <IconMapPin className="mr-2 inline-block" />{" "}
-                  <p className="inline-block cursor-default">Italy</p>
-                </span>
-
-                <span className="flex">
-                  <IconClock className="mr-2 inline-block " />{" "}
-                  <p className="inline-block cursor-default">
-                    UTC +01:00
-                  </p>
-                </span>
-              </div>
-            </div>
-            <div className="m-5 flex w-[26rem] rounded-md border-2 p-5 text-justify">
-              <div>
-                <h1 className="mb-2 text-xl">About Me</h1>
-                <p className="text-md">
-                  Hi! My name is Thebigbot, and I am a web developer with 2
-                  years of experience in the field. I specialize in React ecc..,
-                  and have a passion for creating intuitive and user-friendly
-                  websites and applications. Contact me for any question! :)
-                </p>
               </div>
             </div>
           </div>
@@ -163,4 +141,4 @@ const Home: NextPage = () => {
   );
 };
 
-export default Home;
+export default HomeMobile;
