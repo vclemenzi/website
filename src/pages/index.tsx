9925717ea9533d9104e64544c944b56190d2bb47
Thebigbot <77632836+athebigbot@users.noreply.github.com>
2023-02-25 18:50:26 +0100

    feat: new style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,81 +1,52 @@
-import { IconBrandGithub, IconAt } from "@tabler/icons";
 import { type NextPage } from "next";
 import Head from "next/head";
+
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Thebigbot</title>
+        <title>Valerio Clemenzi</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <div className="hidden lg:block">
-          <div className="flex min-h-screen flex-row items-center justify-center">
-            <div className="mr-20">
-              <h1 className="text-4xl">Hello! I&apos;m Thebigbot</h1>
-              <p className="text-md mt-8 leading-loose text-slate-300">
-                Hello! My name is Valerio, but on the internet, I go by
-                Thebigbot. <br />
-                I love programming and using computers in general, and
-                haven&apos;t touched grass in a while. <br />
-                My favorite technologies are Typescript because my code has to
-                always be typesafe, <br />
-                Nextjs because I want a fantastic routing system, <br />
-                and Tailwindcss to make my websites always look beautiful.{" "}
-                <br />
-                But Rust is also not bad! Want to contact me? Don&apos;t
-                hesitate to do so! <br />
-              </p>
-              <div className="flex">
-                <IconBrandGithub
-                  onClick={() => window.open("https://github.com/athebigbot")}
-                  className="m-3 ml-0 h-8 w-8 cursor-pointer hover:scale-110"
-                />
-                <IconAt
-                  onClick={() =>
-                    (window.location.href = "mailto:hello@thebigbot.dev")
-                  }
-                  className="m-3 h-8 w-8 cursor-pointer hover:scale-110"
-                />
-              </div>
-            </div>
-            <div>
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src="/favicon.ico" alt="" className="h-60 w-60" />
-            </div>
+        <div className="flex h-[45vh] items-center justify-center bg-[#ffc700] p-10">
+          <div className="mr-36 animate-fall border-2 border-r-[6px] border-b-[6px] border-black bg-[#f3763d] p-6 drop-shadow-2xl">
+            <h1 className="text-4xl font-semibold">Valerio Clemenzi</h1>
+            <p>A young web developer</p>
+          </div>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}
+            <img
+              src="/favicon.ico"
+              className="h-44 w-44 animate-fall rounded-full border-8 border-green-600"
+            />
           </div>
         </div>
-        <div className="sm:visible md:hidden lg:hidden xl:hidden 2xl:hidden">
-          <div className="flex min-h-screen flex-row items-center justify-center">
-            <div className="p-8">
-              <h1 className="text-center text-4xl">
-                Hello! I&apos;m Thebigbot
-              </h1>
-              <p className="text-md mt-8 text-justify leading-loose text-slate-300 md:mr-14 md:ml-14">
-                Hello! My name is Valerio, but on the internet, I go by
-                Thebigbot. I love programming and using computers in general,
-                and haven&apos;t touched grass in a while. My favorite
-                technologies are Typescript because my code has to always be
-                typesafe, Nextjs because I want a fantastic routing system, and
-                Tailwindcss to make my websites always look beautiful. But Rust
-                is also not bad! Want to contact me? Don&apos;t hesitate to do
-                so!
-              </p>
-              <div className="mt-4 flex justify-center">
-                <IconBrandGithub
-                  onClick={() => window.open("https://github.com/athebigbot")}
-                  className="m-3 ml-0 h-10 w-10 cursor-pointer hover:scale-110"
-                />
-                <IconAt
-                  onClick={() =>
-                    (window.location.href = "mailto:hello@thebigbot.dev")
-                  }
-                  className="m-3 h-10 w-10 cursor-pointer hover:scale-110"
-                />
-              </div>
-            </div>
+        <div className="flex h-[10vh] items-center justify-center bg-[#5551ff] p-10">
+          <button className="m-3 border-2 border-r-[6px] border-b-[6px] border-black bg-white p-2 pl-12 pr-12 hover:mr-4 hover:border-r-2 hover:border-b-2">
+            GitHub
+          </button>
+          <button className="m-3 border-2 border-r-[6px] border-b-[6px] border-black bg-white p-2 pl-10 pr-10 hover:mr-4 hover:border-r-2 hover:border-b-2">
+            Discord
+          </button>
+          <button className="m-3 border-2 border-r-[6px] border-b-[6px] border-black bg-white p-2 pl-10 pr-10 hover:mr-4 hover:border-r-2 hover:border-b-2">
+            Contact
+          </button>
+        </div>
+        <div className="h-[45vh] justify-center bg-[#c7b9ff] p-10">
+          <div className="h-[18vh] border-2 border-r-[6px] border-b-[6px] border-black bg-[#0fa958] p-4 ml-28 mr-28">
+            <h1 className="text-4xl font-semibold mb-4">About Me!</h1>
+            <p>
+              Hello! My name is Valerio, but on the internet. I love programming
+              and using computers in general, and haven&apos;t touched grass in
+              a while. My favorite technologies are Typescript because my code
+              has to always be typesafe, Nextjs because I want a fantastic
+              routing system, and Tailwindcss to make my websites always look
+              beautiful. But Rust is also not bad! Want to contact me?
+              Don&apos;t hesitate to do so!
+            </p>
           </div>
         </div>
       </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,30 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    animation: {
+      fall: 'fall 1s ease-out forwards',
+      'bounce-slow': 'bounce 2s infinite'
+    },
+    keyframes: {
+      fall: {
+        '0%': {
+          transform: 'translateY(-100%)'
+        },
+        '100%': {
+          transform: 'translateY(0)'
+        },
+        bounce: {
+          '0%, 100%': {
+            transform: 'translateY(-25%)',
+            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)'
+          },
+          '50%': {
+            transform: 'none',
+            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)'
+          }
+        }
+      }
+    }
   },
   plugins: [],
 };
