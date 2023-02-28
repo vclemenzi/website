71255defafaf8e515ec754bd61b28a14299b75d4
sean <126618424+seanseanseanseanseanseansean@users.noreply.github.com>
2023-03-01 00:23:13 +0100

    fixed tailwind config inconsistencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,29 +4,29 @@ module.exports = {
   theme: {
     extend: {},
     animation: {
-      fall: 'fall 1s ease-out forwards',
-      'bounce-slow': 'bounce 2s infinite'
+      "fall": "fall 1s ease-out forwards",
+      "bounce-slow": "bounce 2s infinite",
     },
     keyframes: {
       fall: {
-        '0%': {
-          transform: 'translateY(-100%)'
+        "0%": {
+          transform: "translateY(-100%)",
         },
-        '100%': {
-          transform: 'translateY(0)'
+        "100%": {
+          transform: "translateY(0)",
         },
         bounce: {
-          '0%, 100%': {
-            transform: 'translateY(-25%)',
-            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)'
+          "0%, 100%": {
+            transform: "translateY(-25%)",
+            animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
           },
-          '50%': {
-            transform: 'none',
-            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)'
-          }
-        }
-      }
-    }
+          "50%": {
+            transform: "none",
+            animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
