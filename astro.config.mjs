da712a55dc88f178051433ce283b434fbfc702f6
vclemenzi <vclemenzi@users.noreply.github.com>
2023-09-11 18:55:53 +0200

    fix: cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare({
+    mode: 'directory',
+  })
 });
