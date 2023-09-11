22a4176ff6af0e0e062f945001cbeaf8703f1de6
vclemenzi <vclemenzi@users.noreply.github.com>
2023-09-11 19:05:19 +0200

    moved to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: cloudflare({
-    mode: 'directory',
-  })
+  adapter: netlify()
 });
