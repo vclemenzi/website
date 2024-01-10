81a4479b8cc194b90cbc2b339c10b42fc31e324c
vclemenzi <vclemenzi@users.noreply.github.com>
2024-01-10 19:52:47 +0100

    chore(deploy): vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: cloudflare({ mode: "directory" })
+  adapter: vercel()
 });
