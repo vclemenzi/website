8795725413b2dea3077bb53ca6f0392f1155f889
vclemenzi <vclemenzi@users.noreply.github.com>
2024-01-10 19:32:13 +0100

    chore(deploy): pages adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: cloudflare({ mode: "directory" })
 });
